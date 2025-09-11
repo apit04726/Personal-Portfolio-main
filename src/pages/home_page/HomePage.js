30a0f4bfe25a40c7a7f7f3b6c7a5cbab7bf1dea7
vishal baria <vishalbaria7096@gmail.com>
2025-09-11 15:31:46 +0530

    first commit

---
+++ src/pages/home_page/HomePage.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../../Particle";
+import Typing from "./Typing";
+import Tilt from "react-parallax-tilt";
+import AboutmeContainer from "../../components/aboutme/aboutmeContainer";
+import Fade from "react-reveal/Fade";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import { FaLinkedinIn, FaFacebookF, FaGoogle } from "react-icons/fa";
+import Contactus from "../../components/contactus/Contactus";
+import Projects from "../../components/projects/Projects";
+
+export default function Home() {
+  return (
+    <section className="home-section">
+      <Container fluid id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={8} className="home-header">
+              <h1
+                style={{ paddingBottom: 15, fontSize: "175px" }}
+                className="heading"
+              >
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> Mr Vishal Baria</strong>
+              </h1>
+
+              <div className="typing-homeclass">
+                <Typing />
+              </div>
+              <div className="sociallink-home">
+                <ul className="homeaboutsociallinks">
+                  <li className="socialicons">
+                    <a
+                      href="https://github.com/MD-MAFUJUL-HASAN"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour  homesocialicons"
+                    >
+                      <AiFillGithub />
+                    </a>
+                  </li>
+                  <li className="socialicons">
+                    <a
+                      href="https://www.linkedin.com/in/md-mafujul-hasan/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour  homesocialicons"
+                    >
+                      <FaLinkedinIn />
+                    </a>
+                  </li>
+                  <li className="socialicons">
+                    <a
+                      href="https://www.instagram.com/_md_mafujul_hasan_/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour homesocialicons"
+                    >
+                      <AiFillInstagram />
+                    </a>
+                  </li>
+                  <li className="socialicons">
+                    <a
+                      href="https://www.facebook.com/mh.tonmoy.13"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour homesocialicons"
+                    >
+                      <FaFacebookF />
+                    </a>
+                  </li>
+                  <li className="socialicons">
+                    <a
+                      href="mailto:mdtonmoy13.mt@gmail.com/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour homesocialicons"
+                    >
+                      <FaGoogle />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+            <Col md={4} style={{ paddingBottom: 20 }}>
+              <div
+                className="img-home-main"
+                style={{ mixBlendMode: "lighten" }}
+              >
+                <Tilt>
+                  <Fade cascade>
+                    <img
+                      src="./profile.png"
+                      alt="home pic"
+                      className="img-fluid"
+                    />
+                  </Fade>
+                </Tilt>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <AboutmeContainer />
+// ...existing code...
+      <Projects />
+      <Contactus />
+    </section>
+  );
+}
