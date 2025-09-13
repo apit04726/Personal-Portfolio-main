86e747240d89f5fe8f0c674141fbe3ec3c69693e
vishal baria <vishalbaria7096@gmail.com>
2025-09-13 19:46:35 +0530

    Add margin to download button on ResumePage for improved spacing

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -34,7 +34,7 @@ function Resume() {
               </Zoom>
             </div>
             <div className="d-flex justify-content-center mt-4">
-              <a href={pdf} download="final-resume.pdf">
+              <a href={pdf} download="final-resume.pdf" style={{marginTop:"50px"}}>
                 <Button variant="primary">
                   <AiOutlineDownload />
                   &nbsp;Download Resume
